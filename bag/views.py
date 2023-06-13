b343b7c211a4f0116f359817fec12db1b36295a9
Zandirin <>
2023-06-13 09:37:36 +0000

    updates shopping bag page with product display

--- bag/views.py
+++ bag/views.py
@@ -8,7 +8,7 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
