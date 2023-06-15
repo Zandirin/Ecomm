d6d2639349f46813dcd2085c2fe0304a41d94549
Zandirin <>
2023-06-15 23:40:01 +0000

    tests confirmation bug 2nd attempt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -218,7 +218,8 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_PASS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # Default primary key field type
