caa457eaa349d790197859c570fb5994646e6682
Zandirin <>
2023-06-15 14:41:55 +0000

    adds confirmation email functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -178,6 +178,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+DEFAULT_FROM_EMAIL = 'ecommercesite@doesntexist.com
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
